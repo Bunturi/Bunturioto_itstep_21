82ed3ac3966e263885e42af0ab9bf6707306f2ac
bunturioto <Bunturioto@gmail.com>
2024-02-28 16:31:47 +0400

    Custom encoder function to handle serialization of Product objects

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import json
 
+
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"name: {self.name}, price: {self.price}, quantity: {self.quantity}"
+
+
+# Custom encoder function to handle serialization of Product objects
+def custom_encoder(obj):
+    if isinstance(obj, Product):
+        return {
+            "name": obj.name,
+            "price": obj.price,
+            "quantity": obj.quantity
+        }
+    return obj
